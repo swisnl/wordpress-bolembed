2151f580e476b5aa8f2cbbd61c001aec1cc7c302
cees <cees@swis.nl>
2017-01-27 11:33:56 +0100

    Initial Revision

---
+++ README.txt
@@ -0,0 +1,23 @@
+=== Bol Embed ===
+Contributors:
+Tags:
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Requires at least: 3.5
+Tested up to: 3.5
+Stable tag: 0.1
+
+TODO
+
+== Description ==
+
+TODO
+
+== Installation ==
+
+Upload the Bol Embed plugin to your wordpress site, and activate the plugin.
+
+== Changelog ==
+
+= 0.1 =
+- Initial Revision
---
+++ bolembed.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @package BolEmbed
+ */
+
+/*
+Plugin Name: Bol Embed
+Plugin URI: https://example.com/
+Description: A plugin to embed products from bol.com.
+Version: 0.1
+Author: SWIS
+Author URI: https://swis.nl
+License: GPLv2 or later
+Text Domain: bolembed
+*/
+
+add_action( 'init', function() {
+  wp_oembed_add_provider( '#^https?://www.bol.com/[a-z]{2}/p/[^/]+/(\d+)/?(\?[^?/]+)?$#', 'https://example.com/oembed', true );
+});
+
+register_activation_hook( __FILE__, function() {
+  global $wpdb;
+
+  $wpdb->query("DELETE FROM {$wpdb->postmeta} WHERE meta_key LIKE '_oembed_%' AND meta_value = '{{unknown}}'");
+});
+
+register_deactivation_hook( __FILE__, function() {
+  global $wpdb;
+
+  $wpdb->query("DELETE FROM {$wpdb->postmeta} WHERE meta_key LIKE '_oembed_%' AND meta_value LIKE '%bolembed%'");
+});
